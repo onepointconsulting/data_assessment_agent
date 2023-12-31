4ed7e83ee30157818f8070353e1b583f1c532695
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-12-31 17:13:05 +0000

    Added sentiment prompts and somem extra fields and tables to the relational database

--- sql/db_setup.sql
+++ sql/db_setup.sql
@@ -10,6 +10,7 @@ CREATE DATABASE data_assessment_questionnaire
 DROP TABLE IF EXISTS public.tb_questionnaire_status;
 DROP TABLE IF EXISTS public.tb_question;
 DROP TABLE IF EXISTS public.tb_topic;
+DROP TABLE IF EXISTS public.tb_sentiment_score;
 
 CREATE TABLE public.tb_topic
 (
@@ -37,8 +38,6 @@ CREATE TABLE public.tb_question
         NOT VALID
 );
 
-
-
 CREATE TABLE public.tb_questionnaire_status
 (
     id serial NOT NULL,
@@ -47,6 +46,22 @@ CREATE TABLE public.tb_questionnaire_status
     question character varying(1024) NOT NULL,
     answer character varying(4096) NULL,
     score integer NULL,
+    affirmative_sentiment_score int,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE public.tb_sentiment_score
+(
+    id int NOT NULL,
+    name character varying(15) NOT NULL
+);
+
+ALTER TABLE tb_sentiment_score ADD CONSTRAINT name_unique UNIQUE (name);
+
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(1, 'very negative');
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(2, 'negative');
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(3, 'ambiguous');
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(4, 'positive');
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(5, 'very positive');
+INSERT INTO public.tb_sentiment_score(id, name) VALUES(6, 'unknown');
 
